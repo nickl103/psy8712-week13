32e1349bd8313cb11acca45213c68f71501819a8
nickl103 <nickl103@umn.edu>
2024-04-28 16:43:32 -0500

    organizing code and providing explanations.

--- R/week13-sql.R
+++ R/week13-sql.R
@@ -18,60 +18,62 @@ dbExecute(conn, "USE cla_tntlab;") #specifying what schema to look into for tabl
 
 #Total number of managers
 total_managers <- "SELECT COUNT(*) AS total_managers
-                  FROM datascience_employees 
-                  INNER JOIN datascience_testscores ON datascience_employees.employee_id = datascience_testscores.employee_id;"
+                  FROM datascience_employees AS employees
+                  INNER JOIN datascience_testscores AS testscores ON employees.employee_id = testscores.employee_id;"
 #SELECT COUNT gives a count for the number 
-#FROM datascience_employees pulls from the employees table
-#Inner join joins the employees table with the testscores table, only keeping rows that have matching ones from the testscores table
-dbGetQuery(conn, total_managers) #pulling the number
-#549 managers
+#FROM datascience_employees pulls from the employees table labeled as employees
+#Inner join joins the employees table with the testscores table, only keeping rows that have matching ones from the testscores table labeled as testscores
+dbGetQuery(conn, total_managers) #pulling the number 
+
 
 #Total number of unique managers
-total_unique_managers <- "SELECT COUNT(DISTINCT datascience_employees.employee_id) AS total_unique_managers
-                  FROM datascience_employees 
-                  INNER JOIN datascience_testscores ON datascience_employees.employee_id = datascience_testscores.employee_id;"
+total_unique_managers <- "SELECT COUNT(DISTINCT employees.employee_id) AS total_unique_managers
+                  FROM datascience_employees AS employees
+                  INNER JOIN datascience_testscores AS testscores ON employees.employee_id = testscores.employee_id;"
 
 #did COUNT distinct for employee_id column to get the unique managers 
-dbGetQuery(conn, total_unique_managers)
+#FROM to get data from employees table labeled as employees
+#Inner join with testscores table (labeled as testscores) to only use those with testscores
+dbGetQuery(conn, total_unique_managers) #pulling the information
 
 
 
 #Display a summary of the numbers of managers split by location but only include those who were not originally hired 
-summary_by_location <- "SELECT COUNT(datascience_employees.employee_id) AS number_of_managers, datascience_employees.city AS location
-                        FROM datascience_employees
-                        INNER JOIN datascience_testscores ON datascience_employees.employee_id = datascience_testscores.employee_id
+summary_by_location <- "SELECT COUNT(employees.employee_id) AS number_of_managers, employees.city AS location
+                        FROM datascience_employees AS employees
+                        INNER JOIN datascience_testscores AS testscores ON employees.employee_id = testscores.employee_id
                         WHERE manager_hire = 'N'
                         GROUP BY city;"
 #SELECT count to count number of employees labeled as number_of_managers, city labeled as location to show cities
-#FROM datascience_employees to get data from that table
-#Innerjoin to only include those with testscores
+#FROM datascience_employees to get data from that table labeled as employees
+#Innerjoin to only include those with testscores labeled as testscores
 #WHERE manager_hire = N to only include those not hired as managers
 #GROUP BY city to split by location
-dbGetQuery(conn, summary_by_location)
+dbGetQuery(conn, summary_by_location) #pulling the information
 
 
 #Display the mean and sd of number of years of employment split by performance
 
-mean_sd_by_performance <- "SELECT AVG(datascience_employees.yrs_employed) AS mean_yrs_employed, STDDEV(datascience_employees.yrs_employed) AS sd_yrs_employed, performance_group AS performance_group
-                          FROM datascience_employees
-                          INNER JOIN datascience_testscores ON datascience_employees.employee_id = datascience_testscores.employee_id
+mean_sd_by_performance <- "SELECT AVG(employees.yrs_employed) AS mean_yrs_employed, STDDEV(employees.yrs_employed) AS sd_yrs_employed, performance_group AS performance_group
+                          FROM datascience_employees AS employees
+                          INNER JOIN datascience_testscores AS testscores ON employees.employee_id = testscores.employee_id
                           GROUP BY performance_group;"
 #SELECT AVG gives the average of years employed labelled as mean_yrs_employed
 #STDDEV gives the sd of years employed labelled as sd_yrs_employed
 #Displays performance group labeled as performance group
-#FROM datascience_employees to take from that table
-#Innerjoin to only use employees with testscores
-#group ny performance group per instructions
+#FROM datascience_employees to take from that table labeled as employees
+#Innerjoin to only use employees with testscores labeled as testscores
+#group by performance group per instructions
 
-dbGetQuery(conn, mean_sd_by_performance)
+dbGetQuery(conn, mean_sd_by_performance) #pulling the information
 
 
 #Display each manager's location classification, ID, number, and test score, in alphabetical order by location type and then dec order of test score 
-query1 <- "SELECT datascience_employees.employee_id AS employee_id, datascience_testscores.test_score AS test_score, datascience_offices.type AS type
-            FROM datascience_employees
-            INNER JOIN datascience_testscores ON datascience_employees.employee_id = datascience_testscores.employee_id
-            INNER JOIN datascience_offices ON datascience_employees.city = datascience_offices.office
-            ORDER by datascience_offices.office, datascience_testscores.test_score DESC;"
+query1 <- "SELECT employees.employee_id AS employee_id, testscores.test_score AS test_score, offices.type AS type
+            FROM datascience_employees AS employees
+            INNER JOIN datascience_testscores AS testscores ON employees.employee_id = testscores.employee_id
+            INNER JOIN datascience_offices AS offices ON employees.city = offices.office
+            ORDER by offices.type, testscores.test_score DESC;"
 
-dbGetQuery(conn, query1)
+dbGetQuery(conn, query1) #pulling the information
             
