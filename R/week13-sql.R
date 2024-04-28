2f76582724757117339282b228ee223f22e1e0ea
nickl103 <nickl103@umn.edu>
2024-04-28 16:10:15 -0500

    unique managers and summary split by location

--- R/week13-sql.R
+++ R/week13-sql.R
@@ -17,20 +17,31 @@ dbExecute(conn, "USE cla_tntlab;") #specifying what schema to look into for tabl
 ###Analysis
 
 #Total number of managers
-totalmanagers <- "SELECT COUNT(*) 
+total_managers <- "SELECT COUNT(*) AS total_managers
                   FROM datascience_employees 
                   INNER JOIN datascience_testscores ON datascience_employees.employee_id = datascience_testscores.employee_id;"
 #SELECT COUNT gives a count for the number 
 #FROM datascience_employees pulls from the employees table
 #Inner join joins the employees table with the testscores table, only keeping rows that have matching ones from the testscores table
-dbGetQuery(conn, totalmanagers) #pulling the number
+dbGetQuery(conn, total_managers) #pulling the number
 #549 managers
 
 #Total number of unique managers
+total_unique_managers <- "SELECT COUNT(DISTINCT datascience_employees.employee_id) AS total_unique_managers
+                  FROM datascience_employees 
+                  INNER JOIN datascience_testscores ON datascience_employees.employee_id = datascience_testscores.employee_id;"
 
+#did COUNT distinct for employee_id column to get the unique managers 
+dbGetQuery(conn, total_unique_managers)
 
 #Display a summary of the numbers of managers split by location but only include those who were not originally hired 
+summary_by_location <- "SELECT COUNT(datascience_employees.employee_id) AS number_of_managers, datascience_employees.city AS location
+                    FROM datascience_employees
+                     INNER JOIN datascience_testscores ON datascience_employees.employee_id = datascience_testscores.employee_id
+                    WHERE manager_hire = 'N'
+                    GROUP BY city;"
 
+dbGetQuery(conn, summary_by_location)
 #Display the mean and sd of number of years of employment split by performance
 
 
